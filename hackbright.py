f5eb13e621bdce33609b7961467b4cb9a60d2b49
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-29 13:00:29 -0700

    end of morning

--- hackbright.py
+++ hackbright.py
@@ -42,18 +42,52 @@ def make_new_student(first_name, last_name, github):
     Given a first name, last name, and GitHub account, add student to the
     database and print a confirmation message.
     """
-    pass
+
+    QUERY = """
+        INSERT INTO students (first_name, last_name, github)
+            VALUES (:first_name, :last_name, :github)
+        """
+
+    db.session.execute(QUERY, {'first_name': first_name,
+                                'last_name': last_name,
+                                'github': github})
+    db.session.commit()
+
+    print(f"Successfully added student: {first_name} {last_name}")
 
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    pass
+   
+    QUERY = """
+        SELECT title, description
+        FROM projects
+        WHERE title = :title    
+    """
 
+    db_cursor = db.session.execute(QUERY, {'title': title})
+                                            
+
+    row = db_cursor.fetchone()
+
+    print(f"{row[0]} project is {row[1]}")
 
 def get_grade_by_github_title(github, title):
     """Print grade student received for a project."""
-    pass
+    
+    QUERY = """
+        SELECT student_github, project_title, grade
+        FROM grades
+        WHERE project_title = :title 
+        AND student_github = :github
+    """
 
+    db_cursor = db.session.execute(QUERY, {'title': title,
+                                            'github': github})
+
+    row = db_cursor.fetchone()
+
+    print(f"{row[0]}'s project {row[1]} got a {row[2]}")
 
 def assign_grade(github, title, grade):
     """Assign a student a grade on an assignment and print a confirmation."""
